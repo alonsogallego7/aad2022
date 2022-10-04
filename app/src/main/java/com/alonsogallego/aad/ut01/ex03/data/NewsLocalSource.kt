2b52e05c5efa3d87ce452c1aae0ae78ffd0f91d9
alonsogallego7 <alonso.galpac@educa.jcyl.es>
2022-10-04 20:37:18 +0200

    started exercise 4

--- app/src/main/java/com/alonsogallego/aad/ut01/ex03/data/NewsLocalSource.kt
+++ app/src/main/java/com/alonsogallego/aad/ut01/ex03/data/NewsLocalSource.kt
@@ -2,12 +2,17 @@ package com.alonsogallego.aad.ut01.ex03.data
 
 import android.content.SharedPreferences
 import com.alonsogallego.aad.ut01.ex03.domain.News
+import com.alonsogallego.aad.ut01.ex04.SecureSharedPreferences
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.lang.reflect.Type
 
 class NewsLocalSource (private val sharedPref: SharedPreferences) {
 
+    private val gson = Gson()
+
     fun setNews(news: News) {
-        val gson = Gson()
+
         val jsonNews = gson.toJson(news, News::class.java)
 
         val edit = sharedPref.edit()
@@ -25,12 +30,39 @@ class NewsLocalSource (private val sharedPref: SharedPreferences) {
 
     fun setNewsList(newsList: MutableList<News>){
         newsList.forEach{
-
+            setNews(it)
         }
     }
 
+    fun setNewsListV2(newsList: MutableList<News>) {
+        val jsonNews = sharedPref.getString("ALL", "")
+
+    }
+
     fun findAll() : MutableList<News>{
-        //Buscar y devolver todas las noticias
-        return mutableListOf()
+        val newsList = mutableListOf<News>()
+        sharedPref.all.forEach { entry ->
+            val news = gson.fromJson(entry.value as String, News::class.java)
+            newsList.add(news)
+        }
+        return newsList
+    }
+
+    fun findALLV2(): MutableList<News> {
+        val jsonNews = sharedPref.getString("ALL","")
+
+        val typeNews: Type = object : TypeToken<MutableList<News>>() {}.type
+        return gson.fromJson(jsonNews, typeNews)
+    }
+
+    fun findAllKotlin(): MutableList<News> {
+        return sharedPref.all.map { entry ->
+            gson.fromJson(entry.value as String, News::class.java)
+        }.toMutableList()
+    }
+
+    private fun secure() {
+        val secure = SecureSharedPreferences(applicationContext)
+        secure.saveNews(News(1, "News1", "News1", "News1"))
     }
 }
---
+++ app/src/main/java/com/alonsogallego/aad/ut01/ex04/SecureSharedPreferences.kt
@@ -0,0 +1,31 @@
+package com.alonsogallego.aad.ut01.ex04
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+import com.alonsogallego.aad.ut01.ex03.domain.News
+import com.google.gson.Gson
+
+class SecureSharedPreferences(val applicationContext: Context) {
+
+    val gson = Gson()
+    val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+    val mainKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+
+    val sharedPreferences: SharedPreferences = EncryptedSharedPreferences.create(
+        "NAME_FILE",
+        mainKeyAlias,
+        applicationContext,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+
+    fun saveNews(news: News) {
+        val editor = sharedPreferences.edit()
+        editor.putString(news.id.toString(), gson.toJson(news))
+        editor.apply()
+    }
+
+
+}
